503b7f7b63f78875ff817b2d4790bc8739ee39b5
master <cucuohohcooo@gmail.com>
2026-02-24 14:59:45 +0800

    fix: preview

--- plugin/plantuml.lua
+++ plugin/plantuml.lua
@@ -1,6 +1,6 @@
 -- 自动加载的命令定义
 vim.api.nvim_create_user_command("PlantumlPreviewtutxt", function()
-	require("plantuml_preview").run()
+	require("plantuml").run()
 end, {
 	desc = "生成 PlantUML 文本预览并右分屏显示",
 	nargs = 0,
